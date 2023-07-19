30259294474c154d72e30def0e39965f4c324a11
isaacodrosky <isaacodrosky@gmail.com>
2023-07-19 14:13:14 -0400

    ACNH villager added

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+// BACKGROUND IMAGE
 fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
     .then(res => res.json())
     .then(data => {
         const imgUrl = data.urls.regular
         document.body.style.backgroundImage = `url(${imgUrl})`
         document.getElementById('img-author').textContent = `Image by ${data.user.name}`
+    })
+
+// VILLAGER
+const randomNum = Math.floor((Math.random() * 392))
+
+fetch(`https://acnhapi.com/v1a/villagers/${randomNum}`)
+    .then(res => res.json())
+    .then(data => {
+        document.getElementById('acnh').innerHTML = `
+            <h2>Villager of the Day</h2>
+            <img src="${data.icon_uri}" />
+            <p>${data.name["name-USen"]}</p>`
     })
