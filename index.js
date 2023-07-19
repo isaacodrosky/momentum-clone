b097922331a8619e6b5f9cb2cfc5136e8f0e55b7
isaacodrosky <isaacodrosky@gmail.com>
2023-07-19 13:01:54 -0400

    Image API call

---
+++ index.js
@@ -0,0 +1,7 @@
+fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
+    .then(res => res.json())
+    .then(data => {
+        const imgUrl = data.urls.full
+        document.body.style.backgroundImage = `url(${imgUrl})`
+        document.getElementById('img-author').textContent = `Image by ${data.user.name}`
+    })
